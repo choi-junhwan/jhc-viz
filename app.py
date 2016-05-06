2b6205d7bdc8ddbd04190bc328aa471961183219
Junhwan Choi <choi.junhwan@gmail.com>
2016-05-05 20:43:11 -0500

    Start update

--- app.py
+++ app.py
@@ -1,14 +1,37 @@
 from flask import Flask, render_template, request, redirect
+import counts_by_hour
+import map_static
+import map_live
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-  return redirect('/index')
+    return render_template('index.html')
 
-@app.route('/index')
-def index():
-  return render_template('index.html')
+@app.route('/bus_count')
+def bus_count():
+    script, div = counts_by_hour.plot_count_by_hour()
+    return render_template('bus_count.html', script=script, div=div)
+
+
+@app.route('/bus_static')
+def bus_static():
+    nyc = map_static.read_bus_loc()
+    data = nyc[['latitude','longitude']].values.tolist()
+    return render_template('bus_static.html', data=data)
+
+@app.route('/bus_live')
+def bus_live():
+    nyc = map_live.nyc_current()
+    """
+    plotdf = nyc[['MonitoredVehicleJourney_PublishedLineName',
+                  'MonitoredVehicleJourney_VehicleLocation_Latitude',
+                  'MonitoredVehicleJourney_VehicleLocation_Longitude']]
+    """
+    data = nyc[['MonitoredVehicleJourney_VehicleLocation_Latitude',
+                'MonitoredVehicleJourney_VehicleLocation_Longitude']].values.tolist()
+    return render_template('bus_live.html', data=data)
 
 if __name__ == '__main__':
-  app.run(port=33507)
+    app.run(debug=True)
