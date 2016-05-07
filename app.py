8d7a3788858c66c4d7338cf9ade729ca087f0bb7
Junhwan Choi <choi.junhwan@gmail.com>
2016-05-06 23:01:24 -0500

    update file

--- app.py
+++ app.py
@@ -42,5 +42,5 @@ def bus_live():
     return render_template('bus_live.html', data=data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    #app.run(host='0.0.0.0')
+    #app.run(debug=True)
+    app.run(host='0.0.0.0')
